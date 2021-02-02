56afc26167ffa686a9f4c73f39f535e4547122dc
ivangeorgiev97 <georgiev1313@abv.bg>
2021-02-02 20:27:15 +0200

    Add Category and Memory entities, update mobile navigation

--- backend/app/Memory.php
+++ backend/app/Memory.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Memory extends Model 
 {
     protected $fillable = [
-        'title', 'description'
+        'title', 'description', 'category_id'
     ];
 
     protected $hidden = [];
 
 
-    public function categories() 
+    public function category() 
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belognsTo(Category::class);
     }
 }
